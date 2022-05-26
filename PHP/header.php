027d1164d8f6d2d6a4a9997dfd37aae896b396f6
Kieran Mignon <62752413+Lujac@users.noreply.github.com>
2022-05-26 18:09:38 +0200

    Add files via upload

---
+++ PHP/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>La Crepe Au Sud</title>
+  <link rel="stylesheet" href="CSS/style.css">
+</head>
+<header>
+            <img src ="img/logo.svg" alt ="logo creperie" />
+                <img src="img/creperie.png" alt="image" width="430px"/>
+                <div>
+                <nav id="menu">
+  <ul>
+    <li><a href="#Accueil">Accueil</a></li>
+    <li><a href="#Contact">Formule du moment</a></li>
+    <li><a href="#Contact">La Carte</a></li>
+    <li><a href="#Contact">Galerie </a></li>
+    <li><a href="#Contact">Contact </a></li>
+    <li><a href="#Contact">Actualités </a></li>
+
+  </ul>
+</nav>
+</div>
+</header>
+
+<body>
+ 
+
+
+
+</body>
+</html>
